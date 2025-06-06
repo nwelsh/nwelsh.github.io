793f2d683c6f7d598ca9a492ee81771795f1a1d3
nwelsh <newelsh@wisc.edu>
2025-06-06 11:18:31 -0500

    quick revamp

---
+++ Components/BasicContentHero/BasicContentHero.ts
@@ -0,0 +1,12 @@
+import { HeroProps } from '../../Global/Types'
+import React, { ReactElement } from 'react'
+
+export function renderHero(props: HeroProps): ReactElement {
+    const {image, title, description} = props;
+
+
+  return (
+    <div>
+    <p>nicole</p>
+    </div>);
+}
---
+++ Components/BasicContentHero/BasicContentHeroMockData.ts
@@ -0,0 +1,4 @@
+export const basicContentHeroData = {
+    'title': 'Mocked Hero Title',
+    'description': 'This is a mocked description passed through slot.',
+  }
