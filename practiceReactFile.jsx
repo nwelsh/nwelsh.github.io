5b6695dc7e7ae36078e686388a4dc4972b6f4e21
nwelsh <newelsh@wisc.edu>
2025-08-27 10:06:33 -0500

    add new collage

--- practiceReactFile.jsx
+++ practiceReactFile.jsx
@@ -136,3 +136,19 @@ function Counter() {
   }
   
   console.log(sumDigits(42));
+
+  function twoSum(nums, target) {
+    const map = new Map(); // number -> index
+  
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+  
+      if (map.has(complement)) {
+        return [map.get(complement), i];
+      }
+  
+      map.set(nums[i], i);
+    }
+  
+    return []; // shouldn't reach here if exactly one solution exists
+  }
